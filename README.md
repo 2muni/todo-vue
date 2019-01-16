f47c3524ddbddfbffe689833af2672040e8ab6b1
2muni <relesinc@gmail.com>
2019-01-16 20:30:21 +0900

    project init

---
+++ README.md
@@ -0,0 +1,12 @@
+# todo-vue
+
+> Laravel + Vue.js로 구현한 Todo-List의 Frontend 영역입니다.
+
+## imported package
+
+요구되는 패키지는 다음과 같습니다.
+
+* vue-cli
+* vuex
+* sass-loader
+* node-sass
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+   TodoList goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
