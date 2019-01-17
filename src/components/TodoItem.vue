88913d447adcf43f5122edf05351cddaa1a3661a
2muni <relesinc@gmail.com>
2019-01-17 12:12:40 +0900

    Part 1 - CRUD & Components Communication

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item-left">
+      <div v-if="!editing" class="todo-item-label" @dblclick="editTodo" :class="{completed: completed}">{{ title }}</div>
+      <input v-else class="todo-item-edit" type="text" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
+    </div>
+    <span class="remove-item" @click="removeTodo(todo.id)">&times;</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      id: this.todo.id,
+      title: this.todo.title,
+      completed: this.todo.completed,
+      editing: this.todo.editing,
+      beforeEditCache: '',
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.$emit('removeTodo', id)
+    },
+    editTodo() {
+      this.beforeEditCache = this.title
+      this.editing = true
+    },
+    doneEdit() {
+      if (this.title.trim() == '') {
+        this.title = this.beforeEditCache
+      }
+      this.editing = false
+      this.$emit('finishedEdit', {
+        'id': this.id,
+        'title': this.title,
+        'completed': this.completed,
+        'editing': this.editing,
+      })
+    },
+    cancelEdit() {
+      this.title = this.beforeEditCache
+      this.editing = false
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
+  }
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  &:focus {
+    outline: none;
+  }
+}
+</style>
+
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,64 @@
 <template>
  <div>
-   TodoList goes here
+   <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" @removeTodo="removeTodo" />
   </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem'
+
 export default {
   name: 'TodoList',
+  components: {
+    TodoItem,
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      idForTodo: 2,
+      todos: [
+        {
+          'id': 0,
+          'title': 'Finish Vue Study',
+          'completed': false,
+          'editing': false,
+        },
+        {
+          'id': 1,
+          'title': 'Take over world',
+          'completed': false,
+          'editing': false,
+        },
+      ]
     }
-  }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+        editing: false,
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo(id) {
+      this.todos.splice(id, 1)
+    }
+  },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  &:focus {
+    outline: 0;
+  }
+}
 </style>
